9e2e1c4fff6e2288ff1e6ff76af17823dab1ccbe
Roman <66545463+dumbcirno@users.noreply.github.com>
2026-05-05 22:07:50 +0700

    bwabwa

--- home/mami/fastfetch/default.nix
+++ home/mami/fastfetch/default.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 {
-  xdg.configFile."fastfetch/logo.txt".source =
-    pkgs.runCommand "fastfetch-logo.txt" { } ''
+  xdg.configFile."fastfetch/logo.sixel".source =
+    pkgs.runCommand "fastfetch-logo.sixel" { } ''
       ${pkgs.chafa}/bin/chafa \
+        --format sixel \
         -s 34x16 \
-        --colors 256 \
-        --dither diffusion \
-        --symbols braille \
         ${./fetch.png} > "$out"
     '';
 
@@ -15,7 +13,7 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        source = "${config.xdg.configHome}/fastfetch/logo.txt";
+        source = "${config.xdg.configHome}/fastfetch/logo.sixel";
         type = "file-raw";
         padding = {
           top = 2;
