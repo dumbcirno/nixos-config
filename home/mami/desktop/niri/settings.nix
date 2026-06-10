4f1611203f249bc2b754430ffa895b20ae35c31e
dumbcirno <66545463+dumbcirno@users.noreply.github.com>
2026-06-11 06:16:48 +0700

    5 → 1

--- home/mami/desktop/niri/settings.nix
+++ home/mami/desktop/niri/settings.nix
@@ -58,7 +58,7 @@ in
       }
       { argv = [ "mako" ]; }
       { sh = "sleep 2; exec ${launchWaybar}"; }
-      { sh = "sleep 5; exec ${lib.getExe pkgs.waybar}"; }
+      { sh = "sleep 1; exec ${lib.getExe pkgs.waybar}"; }
       { argv = [ "elephant" ]; }
       { argv = [ "walker" "--gapplication-service" ]; }
       { argv = [ "foot" ]; }
