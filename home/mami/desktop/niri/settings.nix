b3a15c6b5482c22b52ff72762f7f2db210cb3aff
dumbcirno <66545463+dumbcirno@users.noreply.github.com>
2026-06-11 06:10:23 +0700

    waybar

--- home/mami/desktop/niri/settings.nix
+++ home/mami/desktop/niri/settings.nix
@@ -6,10 +6,6 @@ let
     inherit lib pkgs;
     waybarPackage = linuxUnstablePkgs.waybar;
   });
-  launchFootDelayed = lib.getExe (pkgs.writeShellScript "launch-foot-delayed" ''
-    ${pkgs.coreutils}/bin/sleep 2
-    exec ${lib.getExe pkgs.foot}
-  '');
   workspaceFocusBinds = lib.listToAttrs (
     map (x: {
       name = "Mod+${toString (lib.mod x 10)}";
@@ -65,7 +61,7 @@ in
       { argv = [ "elephant" ]; }
       { argv = [ "walker" "--gapplication-service" ]; }
       { argv = [ "foot" ]; }
-      { argv = [ launchFootDelayed ]; }
+      { sh = "sleep 2; exec ${lib.getExe pkgs.foot}"; }
     ];
 
     input = {
