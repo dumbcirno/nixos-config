62c6298a4eee204155b9ebbc30533dd7cd6dd6d0
dumbcirno <dumbcirno>
2026-07-14 12:58:51 +0300

    rm darwin inputs

--- flake.nix
+++ flake.nix
@@ -20,28 +20,8 @@
       url = "github:abenz1267/walker";
       inputs.nixpkgs.follows = "nixpkgsUnstable";
     };
-
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
   };
-
-  outputs = { nixpkgs, nixpkgsUnstable, home-manager, nix-colors, niri, walker, nix-darwin, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ... }:
+  outputs = { nixpkgs, nixpkgsUnstable, home-manager, nix-colors, niri, walker, ... }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
